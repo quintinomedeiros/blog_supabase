a19e299ba1bfbed3df8e13f53f44a6913b742371
quintinomedeiros <quintinomedeiros@yaho.com.br>
2024-09-27 11:53:16 -0300

    desafio_posts

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://sazglmdjdfqzxrdqigjq.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhemdsbWRqZGZxenhyZHFpZ2pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NDMyOTgsImV4cCI6MjA0MzAxOTI5OH0.lkNscXc7K1nNWcXo8hDbydhfUuZOWCaXLWxRKKY7Y_E",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhemdsbWRqZGZxenhyZHFpZ2pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NDMyOTgsImV4cCI6MjA0MzAxOTI5OH0.lkNscXc7K1nNWcXo8hDbydhfUuZOWCaXLWxRKKY7Y_E"
     }
 })
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,7 +1,8 @@
 import { api } from '../services/api'
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+    const {data} = await api.get('/posts');
+    console.log('Posts data:', data);
 
     if(data){
         return data;
@@ -11,9 +12,19 @@ export const getPosts = async () => {
 }
 
 export const getPostBySlug = async (id) => {
-
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
-}
+    try {
+      // Faz a requisição para buscar o post específico pelo id
+      const { data } = await api.get(`/posts?id=eq.${id}`);
+  
+      // Verifica se algum dado foi retornado
+      if (data && data.length > 0) {
+        return data[0]; // Retorna o primeiro post encontrado, pois a API retorna um array
+      } else {
+        console.log(`Nenhum post encontrado com o id: ${id}`);
+        return null;
+      }
+    } catch (error) {
+      console.error(`Erro ao buscar o post com o id: ${id}`, error);
+      return null;
+    }
+  };
